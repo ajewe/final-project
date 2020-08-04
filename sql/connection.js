c4bade6f107e2e8023d4a65502699fdd3bd11c8c
ajewe <ajewe@utexas.edu>
2020-08-04 14:10:53 -0500

    update connection

--- sql/connection.js
+++ sql/connection.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql')
+const dotenv = require('dotenv')
+dotenv.config();
 
 class Connection {
   constructor() {
@@ -7,9 +9,9 @@ class Connection {
 
       const config = {
         connectionLimit: 100,
-        host: 'ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'cf32f6i5cuoo8sz4',
-        password: 'jb7d7pse0ka4rq5g',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
         database: 'aqdsdg3f64t7x685',
         multipleStatements: true
       }
