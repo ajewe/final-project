56839ae60753ca762251a2e06a3a870faa1a8a7d
ajewe <ajewe@utexas.edu>
2020-08-05 11:41:26 -0500

    cleaned up users router and controllers

--- controllers/users.js
+++ controllers/users.js
@@ -3,24 +3,6 @@ const bcrypt = require('bcrypt')
 const pool = require('../sql/connection')
 const { handleSQLError } = require('../sql/error')
 
-const getAllUsers = (req, res) => {
-  pool.query("SELECT * FROM users", (err, rows) => {
-    if (err) return handleSQLError(res, err)
-    return res.json(rows);
-  })
-}
-
-// Maybe in settings, would want to get user info
-// const getUserById = (req, res) => {
-//   let sql = "SELECT * FROM users WHERE id = ?"
-//   sql = mysql.format(sql, [ req.params.id ])
-
-//   pool.query(sql, (err, rows) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.json(rows);
-//   })
-// }
-
 const checkIfExistingUser = (req, res, cb) => {
   const { email } = req.body
   let sql = "SELECT * FROM users WHERE email = ?"
@@ -63,32 +45,6 @@ const createUser = (req, res) => {
   })
 }
 
-// const updateUserById = (req, res) => {
-//   const { firstName, lastName, email, lab_id } = req.body
-//   let sql = "UPDATE users SET first_name = ?, last_name = ?, email = ?, lab_id = ? WHERE id = ?"
-//   sql = mysql.format(sql, [ firstName, lastName, email, lab_id, req.params.id ])
-
-//   pool.query(sql, (err, res) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.status(204).json();
-//   })
-// }
-
-//delete user or just update as inactive user?
-// const deleteUserById = (req, res) => {
-//   let sql = "DELETE FROM users WHERE id = ?"
-//   sql = mysql.format(sql, [ req.params.id ])
-
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
-//   })
-// }
-
 module.exports = {
-  getAllUsers,
-  // getUserById,
-  createUser,
-  // updateUserById,
-  // deleteUserById
+  createUser
 }
--- routers/users.js
+++ routers/users.js
@@ -1,19 +1,8 @@
 const express = require('express')
 const usersController = require('../controllers/users')
-// const { authenticate } = require('../middleware')
 const router = express.Router()
-
-// router.get('/', authenticate, usersController.getAllUsers)
-
-//get rid of this eventually
-router.get('/', usersController.getAllUsers)
-
-// router.get('/:id', usersController.getUserById)
 
 //sign-up, check if email is already registered, if not, create user
 router.post('/create', usersController.createUser)
-
-// router.put('/update/:id', usersController.updateUserById)
-// router.delete('/:first_name', authenticate, usersController.deleteUserByFirstName)
 
 module.exports = router
